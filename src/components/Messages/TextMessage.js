b35910812b34e7d12f368a73d2eb09e2f086ffca
Daniel Ranchal <daniranchal@wealize.digital>
2021-05-06 11:41:20 +0200

    Justify text

--- src/components/Messages/TextMessage.js
+++ src/components/Messages/TextMessage.js
@@ -3,12 +3,22 @@ import MarkdownView from 'react-showdown'
 
 
 const TextMessage = (props) => {
+  const removeParagraphs = () => {
+    return [{
+      type: 'output',
+      filter: (text) => {
+        return text.replace(/<\/?p[^>]*>/g, '')
+      }
+    }]
+  }
+
   return (
     <MarkdownView
       className="sc-message--text"
       markdown={props.data.text}
       options={{ simplifiedAutoLink: true, openLinksInNewWindow: true }}
       flavor={'github'}
+      extensions={removeParagraphs}
     />
   )
 }
